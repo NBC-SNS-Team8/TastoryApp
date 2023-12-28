ba93c039197245cb9e0f50e6dbb1af7c0c631b1c
nhkim <nhkim1529@gmail.com>
2023-12-28 20:24:27 +0900

    Feat: MyPage에 User 객체 받아오기

--- app/src/main/java/com/nhkim/tastory/MyPageActivity.kt
+++ app/src/main/java/com/nhkim/tastory/MyPageActivity.kt
@@ -3,7 +3,9 @@ package com.nhkim.tastory
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 
 class MyPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +13,15 @@ class MyPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_my_page)
 
         val xIcon = findViewById<ImageView>(R.id.x_icon)
+        val blogName = findViewById<TextView>(R.id.tv_blog_name)
+        val blogId = findViewById<TextView>(R.id.tv_blog_id)
+
+        val receivedUser = intent.getParcelableExtra<User>("USER")
+        blogName.text = receivedUser?.name
+        blogId.text = receivedUser?.id
 
         xIcon.setOnClickListener {
-            val intent = Intent(this, FeedActivity::class.java)
-            startActivity(intent)
+            finish()
         }
     }
 
