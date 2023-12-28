605a0dcaaa5664a7129ef83168a904ebec4fb15e
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2023-12-28 10:37:23 +0900

    signupActivity

--- app/src/main/java/com/nhkim/tastory/LoginActivity.kt
+++ app/src/main/java/com/nhkim/tastory/LoginActivity.kt
@@ -1,11 +1,36 @@
 package com.nhkim.tastory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.constraintlayout.widget.ConstraintLayout
 
+lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        
+        val editTextLoginId = findViewById<EditText>(R.id.et_login_id)
+        val editTextLoginPw = findViewById<EditText>(R.id.et_login_pw)
+        val btnLogin = findViewById<ConstraintLayout>(R.id.cl_login_btn)
+        val btnSignupPage = findViewById<ConstraintLayout>(R.id.cl_login_signup_btn)
+        
+        //activityResultLauncher
+        
+        btnLogin.setOnClickListener {
+            if (editTextLoginId.text.trim().isEmpty() || editTextLoginPw.text.trim().isEmpty()) {
+                Toast.makeText(this, getString(R.string.toast_empty), Toast.LENGTH_SHORT).show()
+            }
+        }
+        
+        btnSignupPage.setOnClickListener { 
+            
+        }
+        
+        
     }
 }
