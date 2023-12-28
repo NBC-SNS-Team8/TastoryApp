0febe499929997ec3f7a2d775d9c7584090d5783
YoungjeongChoi <151485887+YoungjeongChoi@users.noreply.github.com>
2023-12-28 14:42:14 +0900

    Chore: 다른 파일 업데이트

--- app/src/main/java/com/nhkim/tastory/DetailActivity.kt
+++ app/src/main/java/com/nhkim/tastory/DetailActivity.kt
@@ -1,11 +1,24 @@
 package com.nhkim.tastory
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val btn = findViewById<ImageView>(R.id.imageView)
+
+        btn.setOnClickListener {
+            val intent = Intent(this, FeedActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
     }
 }
