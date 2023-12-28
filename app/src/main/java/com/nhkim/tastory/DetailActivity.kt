fcdb4958c41d5fd1117b2c4543f0b1c9f614c558
JungMok Yoo <57724499+JungMokYoo@users.noreply.github.com>
2023-12-28 20:52:07 +0900

    detailpage archive

--- app/src/main/java/com/nhkim/tastory/DetailActivity.kt
+++ app/src/main/java/com/nhkim/tastory/DetailActivity.kt
@@ -1,16 +1,26 @@
 package com.nhkim.tastory
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        var btn =findViewById<ImageView>(R.id.imageView)
 
+
+        btn.setOnClickListener {
+            //finish()
+            val intent = Intent(this, FeedActivity::class.java)
+            startActivity(intent)
+        }
+
+        val strData = intent.getStringArrayExtra("clickedPicture")
+        val editPicture = findViewById<ImageView>(R.id.imageView3)
 
 
 
